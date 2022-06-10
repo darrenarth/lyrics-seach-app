411fafab3dcd14b29c27336335f7d61996f22efa
darrenarth <99767747+darrenarth@users.noreply.github.com>
2022-06-10 12:49:32 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,65 @@
+const form = document.getElementById("form");
+const search = document.getElementById("search");
+const result = document.getElementById("result");
+
+const apiURL="https://api.lyrics.ovh";
+//get search value
+form.addEventListener("submit", e => {
+    e.preventDefault();
+    searchValue = search.value.trim();
+
+    if (!searchValue) {
+        alert("nothing to search");
+    } else {
+        beginSearch(searchValue);
+
+    } 
+})
+// search function
+async function beginSearch(searchValue) {
+    const searchResult = await fetch( ` ${apiURL}/suggest/${searchValue}`);
+    const data = await searchResult.json();
+
+    displayData(data);
+}
+//display search searchResult
+function displayData(data) {
+    result.innerHTML = `
+    <ul class="songs" >
+        ${data.data
+        .map(song=> ` <li>
+                        <div>
+                            <strong>${song.artist.name}</strong> - ${song.title}
+                        </div>
+                        <span data-artist="${song.artist.name}" data-songTitle="${song.title}">Get lyrics</span>
+                      </li>`
+        )
+        .join('')}
+        </ul>
+        `;
+        }
+
+// event listener in get lyrics button
+result.addEventListener('click', e=>{
+    const clickedElement = e.target;
+
+    //checking clicked element is button or not
+    if (clickedElement.tagName === 'SPAN') {
+        const artist = clickedElement.getAttribute('data-artist');
+        const songTitle = clickedElement.getAttribute('data-songTitle');
+
+        getLyrics(artist, songTitle)
+    }
+})
+
+// get lyrics for song
+async function getLyrics(artist,songTitle) {
+    const response = await fetch(`${apiURL}/v1/${artist}/${songTitle}`);
+    const data = await response.json();
+    
+    const lyrics = data.lyrics.replace(/(\r\n|\r|\n)/g, '<br>');
+
+    result.innerHTML = `<h2><strong>${artist}</strong> - ${songTitle}</h2> 
+    <p>${lyrics}</p>`;
+
+}
